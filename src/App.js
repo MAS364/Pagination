ca1f9d816e0d9a4e88a9120ebd26ad1a758ece2c
Mohammad Asim <mohammadasim@Mohammads-MacBook-Air.local>
2024-01-13 23:53:13 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchData = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const fetcheddata = await res.json();
+    setData(fetcheddata.products);
+    console.log(fetcheddata);
+    console.log(fetcheddata.products);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const setPageHandler = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= data.length / 10)
+      setPage(selectedPage);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="products">
+        {data.slice(page * 10 - 10, page * 10).map((item) => (
+          <span className="products-single">
+            <img src={item.thumbnail} alt={item.title} />
+            <span>{item.title}</span>
+          </span>
+        ))}
+      </div>
+
+      <div className="pagination">
+        <span onClick={() => setPageHandler(page - 1)}>◀︎</span>
+        {[...Array(data.length / 10)].map((_, i) => (
+          <span
+            className={page === i + 1 ? "page-selected" : ""}
+            onClick={() => setPageHandler(i + 1)}
+          >
+            {i + 1}
+          </span>
+        ))}
+        <span onClick={() => setPageHandler(page + 1)}>►</span>
+      </div>
     </div>
   );
 }
-
-export default App;
